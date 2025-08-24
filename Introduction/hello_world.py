83959af8ca291d1d2109fd4a201453caf5864674
Sindhu <nandhisindhu2110@gmail.com>
2025-08-24 15:01:30 +0530

    add Introduction folder with Hello World solution

--- Introduction/hello_world.py
+++ Introduction/hello_world.py
@@ -1,4 +1,3 @@
-#Problem statement: https://www.hackerrank.com/challenges/py-hello-world/problem
+#Problem statement: https://www.hackerrank.com/challenges/py-hello-world/problem 
 
-print("Hello World!") #Use built-in `print()` function to display text
-
+print("Hello World!") #Use built-in print() function to display text
